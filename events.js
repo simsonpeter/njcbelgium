9378d6005ddfe9c5b756a09f8041cd8bbecdc207
simsonpeter <simsonpeter@gmail.com>
2025-04-13 19:24:55 +0200

    Update events.js

--- events.js
+++ events.js
@@ -1,1 +1,15 @@
 
+[
+         {
+             "title": "Good Friday",
+             "date": "2025-04-18",
+             "time": "18:00 PM",
+             "description": "Join us for our Good Friday service"
+         },
+         {
+             "title": "Bible Study Workshop",
+             "date": "2025-05-15",
+             "time": "6:00 PM",
+             "description": "Deep dive into biblical teachings with Pastor Simson"
+         }
+     ]
